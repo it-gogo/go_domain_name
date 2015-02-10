f7e653e4c8ae8f2860d9cb2f1e3788e37ea0f440
641484739 <641484739@qq.com>
2015-02-10 17:45:46 +0800

    Signed-off-by: 641484739 <641484739@qq.com>

--- go_domain_name/src/com/go/email/SendEmail.java
+++ go_domain_name/src/com/go/email/SendEmail.java
@@ -19,13 +19,24 @@ public class SendEmail{
 	 * @param text 邮件邮箱内容
 	 * @throws Exception
 	 */
-	public void sendEmail(String email,String title,String text) throws Exception{
-		MimeMessage mime=new JavaMailSenderImpl().createMimeMessage();
-		MimeMessageHelper messageHelper = new MimeMessageHelper(mime,true,"utf-8");  //设置utf-8或GBK编码，否则邮件会有乱码  
-		messageHelper.setTo(email);//接收邮件邮箱
-		messageHelper.setFrom(sender.getUsername());//发送邮件邮箱
-		messageHelper.setSubject(title);//标题
-		messageHelper.setText(text, true);
-		sender.send(mime);
+	public void sendEmail(final String email,final String title,final String text) throws Exception{
+		Runnable r = new Runnable() {
+			@Override
+			public void run() {
+				try {
+					MimeMessage mime=new JavaMailSenderImpl().createMimeMessage();
+					MimeMessageHelper messageHelper = new MimeMessageHelper(mime,true,"utf-8");  //设置utf-8或GBK编码，否则邮件会有乱码  
+					messageHelper.setTo(email);//接收邮件邮箱
+					messageHelper.setFrom(sender.getUsername());//发送邮件邮箱
+					messageHelper.setSubject(title);//标题
+					messageHelper.setText(text, true);
+					sender.send(mime);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		Thread t=new Thread(r);
+		t.start();
 	}
 }
